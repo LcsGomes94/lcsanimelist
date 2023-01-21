7a136504fa353f0e8b72694b534bfa0d1bd705da
Lucas Gomes <lcsdev94@gmail.com>
2023-01-21 17:11:33 -0300

    fixed layout shift prevention that was missing in mobile userMenu component

--- src/components/UserMenu.tsx
+++ src/components/UserMenu.tsx
@@ -37,10 +37,10 @@ export default function UserMenu() {
                 handleOpenSearchModal()
             }}
                 disabled={router.asPath === '/seasonal'}
-                className={`flex justify-center items-center md:hidden pb-[1px] hover:bg-gray-100 dark:hover:bg-gray-700 h-8 w-8 rounded-full transition-opacity duration-700 ${opacity === 100 ? 'opacity-100' : 'opacity-0'} disabled:cursor-not-allowed disabled:opacity-50`}>
+                className={`flex justify-center items-center md:hidden pb-[1px] hover:bg-gray-100 dark:hover:bg-gray-700 h-8 w-8 rounded-full transition-opacity duration-700 ${opacity === 100 ? 'opacity-100 disabled:opacity-50' : 'opacity-0'} disabled:cursor-not-allowed`}>
                 <SearchIcon className={`h-[22px] w-[22px]`} />
             </button>
-            <button onClick={() => handleOpenUserModal()} className={`flex md:hidden pl-1.5`}>
+            <button onClick={() => handleOpenUserModal()} className={`flex md:hidden pl-1.5 transition-opacity duration-700 ${opacity === 100 ? 'opacity-100' : 'opacity-0'}`}>
                 {session ? <img src={session?.user?.image!} alt='img' className={`rounded-full border border-cyan-600 dark:border-cyan-400 h-8 w-8`} /> : <GitHubIcon />}
             </button>
             <div className={`hidden md:block transition-opacity duration-700 ${opacity === 100 ? 'opacity-100' : 'opacity-0'}`}>
